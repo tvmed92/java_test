a8dd98c79ddb875a2d21d425d207313e9450a38b
tvmed92 <tvmed92@gmail.com>
2019-06-26 23:37:02 +0300

    Created distance calculator for points
    Created program for calculating the distance between two points on the flat

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,5 +1,6 @@
 public class MyFirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
+    public static void main(String[] args) {
+        Point p = new Point(4.1, 5.3, 1.7, 9.4);
+        System.out.println(p.distance());
+    }
 }
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,19 @@
+public class Point {
+    public double x1;
+    public double x2;
+    public double y1;
+    public double y2;
+
+    public Point(double x1, double x2, double y1, double y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+    public double distance() {
+        double x = this.x2 - this.x1;
+        double y = this.y2 - this.y1;
+        return Math.sqrt(x * x + y * y);
+    }
+}
