68072bbd4ba247b4bb7dc814ff3eafa94997093c
tvmed92 <tvmed92@gmail.com>
2019-07-05 00:04:50 +0300

    refactored test: created GroupData class

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -11,57 +11,84 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class GroupCreationTests {
-  private ChromeDriver wD;
+    private ChromeDriver wD;
 
-  @BeforeMethod
-  public void setUp() throws Exception {
-    wD = new ChromeDriver();
-    wD.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wD.get("http://localhost/addressbook/");
-    wD.findElement(By.name("user")).clear();
-    wD.findElement(By.name("user")).sendKeys("admin");
-    wD.findElement(By.name("pass")).clear();
-    wD.findElement(By.name("pass")).sendKeys("secret");
-    wD.findElement(By.xpath("//input[@value='Login']")).click();
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
-    wD.findElement(By.linkText("groups")).click();
-    wD.findElement(By.name("new")).click();
-    wD.findElement(By.name("group_name")).click();
-    wD.findElement(By.name("group_name")).clear();
-    wD.findElement(By.name("group_name")).sendKeys("test_group");
-    wD.findElement(By.name("group_header")).clear();
-    wD.findElement(By.name("group_header")).sendKeys("test_header");
-    wD.findElement(By.name("group_footer")).click();
-    wD.findElement(By.name("group_footer")).clear();
-    wD.findElement(By.name("group_footer")).sendKeys("test_footer");
-    wD.findElement(By.name("submit")).click();
-    wD.findElement(By.linkText("group page")).click();
-    wD.findElement(By.linkText("Logout")).click();
-  }
-
-  @AfterMethod
-  public void tearDown() throws Exception {
-    wD.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wD.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wD = new ChromeDriver();
+        wD.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wD.get("http://localhost/addressbook/");
+        login("admin", "secret");
     }
-  }
 
-  private boolean isAlertPresent() {
-    try {
-      wD.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+    private void login(String username, String password) {
+        wD.findElement(By.name("user")).clear();
+        wD.findElement(By.name("user")).sendKeys(username);
+        wD.findElement(By.name("pass")).clear();
+        wD.findElement(By.name("pass")).sendKeys(password);
+        wD.findElement(By.xpath("//input[@value='Login']")).click();
     }
-  }
+
+    @Test
+    public void testGroupCreation() throws Exception {
+        goToGroupPage();
+        initGroupCreation();
+        fillGroupForm(new GroupData(
+                "test_name",
+                "test_header",
+                "test_footer"));
+        submitGroupCreation();
+        returnToGroupPage();
+        wD.findElement(By.linkText("Logout")).click();
+    }
+
+    private void returnToGroupPage() {
+        wD.findElement(By.linkText("group page")).click();
+    }
+
+    private void submitGroupCreation() {
+        wD.findElement(By.name("submit")).click();
+    }
+
+    private void fillGroupForm(GroupData groupData) {
+        wD.findElement(By.name("group_name")).click();
+        wD.findElement(By.name("group_name")).clear();
+        wD.findElement(By.name("group_name")).sendKeys(groupData.getGroupName());
+        wD.findElement(By.name("group_header")).clear();
+        wD.findElement(By.name("group_header")).sendKeys(groupData.getGroupHeader());
+        wD.findElement(By.name("group_footer")).click();
+        wD.findElement(By.name("group_footer")).clear();
+        wD.findElement(By.name("group_footer")).sendKeys(groupData.getGroupFooter());
+    }
+
+    private void initGroupCreation() {
+        wD.findElement(By.name("new")).click();
+    }
+
+    private void goToGroupPage() {
+        wD.findElement(By.linkText("groups")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        wD.quit();
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            wD.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            wD.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
 }
